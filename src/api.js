4eac5ca5e7c33659b359dd2585b2f99d20e69ce2
IacobIsabela <91394180+IacobIsabela@users.noreply.github.com>
2023-02-06 05:42:05 +0200

    update project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bb89a6534fmshe83255d39481a47p1d75e4jsnf356c8816f99',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"; 
+export const WEATHER_API_KEY = "9c5693d17a011dc15ad210a90f2363d8";
